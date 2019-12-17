037de5dbfec99ccaae9886ac00b0c468cf49e553
Clovis Chanay <>
2019-12-17 12:39:41 +1100

    Implemented forked flask-restplus-patched folder in codebase

--- app/extensions/api/__init__.py
+++ app/extensions/api/__init__.py
@@ -1,20 +1,27 @@
 """
 app/extensions/api/__init__.py
-Initialize flask restplus api
+Initialize overriden Flask-Restplus Api class
+Forked from https://github.com/frol/flask-restplus-server-example flask-restplus-patched folder
 """
-from flask_restplus import Api
+from .api import Api
+from .namespace import Namespace
+from .parameters import Parameters
+from .resource import Resource
+from .model import ModelSchema
+from .http_exceptions import abort, HTTPStatus
+from app.extensions.constants import *
 
 
 api = Api(
     version='1.0.0',
-    title="RefactoredXeroAPI",
-    description="Refactored Xero API, from RefactorThis.",
+    title="ProductAPI",
+    description="Refactored API from RefactorThis",
     ordered=True,
     doc='/doc/'
 )
 
 # Temporary fix concerning https://github.com/noirbizarre/flask-restplus/issues/460
-api.namespaces = []
+api.namespaces.pop(0)
 
 
 def init_app(app, **kwargs):
