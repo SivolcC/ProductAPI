0fb7a077d77c5dc5a079032b229446f945429809
Clovis Chanay <>
2019-12-17 13:55:29 +1100

    Implemented autocommit after each request with exception handling

--- app/extensions/flask_sqlalchemy/__init__.py
+++ app/extensions/flask_sqlalchemy/__init__.py
@@ -22,3 +22,42 @@ class SQLAlchemy(BaseSQLAlchemy):
         assert SQLALCHEMY_DATABASE_URI in app.config and app.config.get(SQLALCHEMY_DATABASE_URI),\
             "SQLALCHEMY_DATABASE_URI must be configured!"
         super(SQLAlchemy, self).init_app(app)
+
+
+        @app.after_request
+        def after_request(response):
+            """
+            Execute commit on db after request if the session has object
+            that needs to be commited.
+            """
+            from app.extensions import db
+            from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+            from app.extensions.api import abort, HTTPStatus
+
+            if response.status_code >= 400:
+                return response
+            if db.session.new or db.session.dirty or db.session.deleted:
+                try:
+                    db.session.commit()
+                except ValueError as e:
+                    db.session.rollback()
+                    abort(
+                        code=HTTPStatus.CONFLICT
+                    )
+                except IntegrityError as e:
+                    db.session.rollback()
+                    abort(
+                        code=HTTPStatus.CONFLICT
+                    )
+                except SQLAlchemyError as e:
+                    db.session.rollback()
+                    abort(
+                        code=HTTPStatus.INTERNAL_SERVER_ERROR
+                    )
+                except Exception as e:
+                    db.session.rollback()
+                    abort(
+                        code=HTTPStatus.INTERNAL_SERVER_ERROR
+                    )
+            # db.session.close()
+            return response
