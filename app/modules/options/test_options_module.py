386a2e816a99565ac8aa64166410aff03c7c2a04
Clovis Chanay <>
2019-12-18 13:05:58 +1100

    Commented unit tests

--- app/modules/options/test_options_module.py
+++ app/modules/options/test_options_module.py
@@ -8,39 +8,43 @@ import json
 
 
 def test_create_product_option(flask_app_client, product_test_one):
+    # Create product (fixture) option
     response = flask_app_client.post('/api/products/' + str(product_test_one.id) + '/options', json={
         "Name":  "Option Test One",
         "Description": "Option for Test product One",
     })
     assert response.status_code == 200
-
+    # Asserts response
     created_option = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert created_option['Name'] == "Option Test One"
     assert created_option['Description'] == "Option for Test product One"
-
+    # Get created optiom
     response = flask_app_client.get('/api/options/' + created_option['Id'])
     assert response.status_code == 200
+    # Asserts values
     created_option = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert created_option['Name'] == "Option Test One"
     assert created_option['Description'] == "Option for Test product One"
-
+    # Delete option
     response = flask_app_client.delete('/api/options/' + created_option['Id'])
     assert response.status_code == 204
 
 
 def test_get_product_option(flask_app_client, product_test_one):
+    # Create product (fixture) option
     response = flask_app_client.post('/api/products/' + str(product_test_one.id) + '/options', json={
         "Name":  "Option Test Two",
         "Description": "Option for Test product Two",
     })
     assert response.status_code == 200
-
+    # Asserts response
     created_option = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert created_option['Name'] == "Option Test Two"
     assert created_option['Description'] == "Option for Test product Two"
-
+    # Get created optiom
     response = flask_app_client.get('/api/options/' + created_option['Id'])
     assert response.status_code == 200
+    # Asserts values
     created_option = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert created_option['Name'] == "Option Test Two"
     assert created_option['Description'] == "Option for Test product Two"
@@ -50,6 +54,7 @@ def test_update_product_option(flask_app_client, product_test_one):
     test_option_name = "Test Option Updated"
     test_option_description = "Test Option description updated"
 
+    # Create product (fixture) option
     response = flask_app_client.post('/api/products/' + str(product_test_one.id) + '/options', json={
         "Name":  "Test Option",
         "Description": "Test Description",
@@ -57,32 +62,33 @@ def test_update_product_option(flask_app_client, product_test_one):
     assert response.status_code == 200
     created_option = json.loads(response.data.decode('utf-8').replace("'", '"'))
 
+    # Update option
     response = flask_app_client.put('/api/options/' + created_option['Id'], json={
         "Name": test_option_name,
         "Description": test_option_description,
     })
     assert response.status_code == 200
-
+    # Asserts values
     updated_option = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert updated_option['Name'] == test_option_name
     assert updated_option['Description'] == test_option_description
-
+    # Get updated option
     response = flask_app_client.get('/api/options/' + updated_option['Id'])
     updated_option = json.loads(response.data.decode('utf-8').replace("'", '"'))
-
+    # Asserts values
     assert response.status_code == 200
     assert updated_option['Name'] == test_option_name
     assert updated_option['Description'] == test_option_description
 
 
 def test_delete_product_option(flask_app_client, product_test_one):
+    # Create product (fixture) option
     response = flask_app_client.post('/api/products/' + str(product_test_one.id) + '/options', json={
         "Name":  "Option Test One",
         "Description": "Option for Test product One",
     })
     assert response.status_code == 200
-
     created_option = json.loads(response.data.decode('utf-8').replace("'", '"'))
-
+    # Delete option
     response = flask_app_client.delete('/api/options/' + created_option['Id'])
     assert response.status_code == 204
--- app/modules/products/test_products_module.py
+++ app/modules/products/test_products_module.py
@@ -17,38 +17,42 @@ def test_create_product(flask_app_client):
     test_product_price = 54.9
     test_product_delivery_price = 19.9
 
+    # Creates a product
     response = flask_app_client.post('/api/products', json={
         "Name": test_product_name,
         "Description": test_product_description,
         "Price": test_product_price,
         "DeliveryPrice": test_product_delivery_price
     })
+    # Asserts response
     assert response.status_code == 200
-
     created_product = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert created_product['Name'] == test_product_name
     assert created_product['Description'] == test_product_description
     assert created_product['Price'] == test_product_price
     assert created_product['DeliveryPrice'] == test_product_delivery_price
 
+    # Get created product
     response = flask_app_client.get('/api/products/' + created_product['Id'])
-    created_product = json.loads(response.data.decode('utf-8').replace("'", '"'))
-
     assert response.status_code == 200
+    # Assert values
+    created_product = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert created_product['Name'] == test_product_name
     assert created_product['Description'] == test_product_description
     assert created_product['Price'] == test_product_price
     assert created_product['DeliveryPrice'] == test_product_delivery_price
 
+    # Delete product
     response = flask_app_client.delete('/api/products/' + created_product['Id'])
     assert response.status_code == 204
 
 
 def test_get_product(flask_app_client, product_test_one):
+    # Get product (fixture)
     response = flask_app_client.get('/api/products/' + str(product_test_one.id))
-    product = json.loads(response.data.decode('utf-8').replace("'", '"'))
-
     assert response.status_code == 200
+    # Asserts values
+    product = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert product['Name'] == "Product Test One"
     assert product['Description'] == "Test product"
     assert product['Price'] == 42
@@ -61,6 +65,7 @@ def test_update_product(flask_app_client, product_test_one):
     test_product_price = 108.9
     test_product_delivery_price = 39.9
 
+    # Update product (fixture)
     response = flask_app_client.put('/api/products/' + str(product_test_one.id), json={
         "Name": test_product_name,
         "Description": test_product_description,
@@ -68,17 +73,18 @@ def test_update_product(flask_app_client, product_test_one):
         "DeliveryPrice": test_product_delivery_price
     })
     assert response.status_code == 200
-
+    # Asserts response
     updated_product = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert updated_product['Name'] == test_product_name
     assert updated_product['Description'] == test_product_description
     assert updated_product['Price'] == test_product_price
     assert updated_product['DeliveryPrice'] == test_product_delivery_price
 
+    # Get updated product
     response = flask_app_client.get('/api/products/' + updated_product['Id'])
-    updated_product = json.loads(response.data.decode('utf-8').replace("'", '"'))
-
     assert response.status_code == 200
+    # Asserts values
+    updated_product = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert updated_product['Name'] == test_product_name
     assert updated_product['Description'] == test_product_description
     assert updated_product['Price'] == test_product_price
@@ -91,6 +97,7 @@ def test_delete_product(flask_app_client):
     test_product_price = 54.9
     test_product_delivery_price = 19.9
 
+    # Create product
     response = flask_app_client.post('/api/products', json={
         "Name": test_product_name,
         "Description": test_product_description,
@@ -99,18 +106,21 @@ def test_delete_product(flask_app_client):
     })
     assert response.status_code == 200
     created_product = json.loads(response.data.decode('utf-8').replace("'", '"'))
+    # Delete product
     response = flask_app_client.delete('/api/products/' + created_product['Id'])
     assert response.status_code == 204
 
 
 def test_get_product_options(flask_app_client, product_test_one):
+    # Create product (fixture) option
     response = flask_app_client.post('/api/products/' + str(product_test_one.id) + '/options', json={
         "Name":  "Option Test One",
         "Description": "Option for Test product One",
     })
-
+    # Get product (fixture) options
     response = flask_app_client.get('/api/products/' + str(product_test_one.id) + '/options')
     assert response.status_code == 200
+    # Asserts values
     product_options = json.loads(response.data.decode('utf-8').replace("'", '"'))
     assert response.status_code == 200
     assert len(product_options['Items']) == product_options['total'] or product_options['per_page']
